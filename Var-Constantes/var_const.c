02482b913728fea7748d78f1f760ab78dcb2ede5
JhullyVitoria <118947407+JhullyVitoria@users.noreply.github.com>
2026-08-28 21:34:27 -0300

    Variáveis e Constantes

--- Var-Constantes/var_const.c
+++ Var-Constantes/var_const.c
@@ -1,1 +1,9 @@
+#define peso 50.67 //constante
 
+#include <stdio.h>
+
+int main(){
+    int idade = 20; //variável
+    const char nome[10] = "jhully"; //constante
+    printf("A %s pesa %.2f kg", nome, peso);
+}
